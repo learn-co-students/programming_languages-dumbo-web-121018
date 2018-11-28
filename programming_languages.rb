f82fe14c27f36f8aceecaf02b070ccdff7561e1d
Menachem Aber <abermendel@gmail.com>
2018-11-28 22:00:56 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,23 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_langs = {}
+  
+  languages.each do |styles, langs|
+    langs.each do |types, value|
+      new_langs[types] = {type: value.values.join, style: []}
+    end
+  end
+  
+  languages.each do |styles, langs|
+    langs.each do |types, value|
+      new_langs[types][:style] << styles
+    end
+  end
+
+  
+  
+  binding.pry
+  new_langs
+  
 end

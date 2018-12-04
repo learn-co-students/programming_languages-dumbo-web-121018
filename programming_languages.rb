7264472e665f08eef9eb84bf8f277b12fe78a641
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-12-04 01:51:41 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |type, language|
+      language.each do |name, info|
+        if new_hash.key?(name)
+          new_hash[name][:style].push(type)
+        else 
+          new_hash[name] = info
+          new_hash[name][:style] = [type]
+        end
+      end
+  end
+  return new_hash
 end
